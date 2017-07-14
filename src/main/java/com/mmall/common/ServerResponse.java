f68cd33de3f341b38462d0dd628cb8e90ff0d672
tangqy <tangqy0.0@gmail.com>
2017-07-14 13:32:35 +0800

    comment class commit

---
+++ src/main/java/com/mmall/common/ServerResponse.java
@@ -0,0 +1,71 @@
+package com.mmall.common;
+
+import java.io.Serializable;
+
+/**
+ * Created by tangqy on 2017/7/13.
+ */
+public class ServerResponse<T> implements Serializable {
+
+    private int status;
+    private String msg;
+    private T data;
+
+    public ServerResponse(int status) {
+        this.status = status;
+    }
+
+    private ServerResponse(int status, String msg) {
+        this.status = status;
+        this.msg = msg;
+    }
+
+    private ServerResponse(int status, T data) {
+        this.status = status;
+        this.data = data;
+    }
+
+    private ServerResponse(String msg, T data) {
+        this.msg = msg;
+        this.data = data;
+    }
+
+    private ServerResponse(int status, String msg, T data) {
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public boolean isSuccess() {
+        return this.status == ResponseCode.SUCCESS.getCode();
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public static <T> ServerResponse<T> createBySuccess() {
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode());
+    }
+
+    public static <T> ServerResponse<T> createBySuccess(T data) {
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), data);
+    }
+
+    public static <T> ServerResponse<T> createBySuccess(String msg, T data) {
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), msg, data);
+    }
+
+    public static <T> ServerResponse<T> createBySuccessMessage(String msg) {
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), msg);
+    }
+
+}
